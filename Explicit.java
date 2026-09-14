7ff4ed1a3f349edbad9db22a837a362580425626
mrchandrakantsahoo-lab <aapka-github-mrchandrakantsahoo@gmail.com>
2026-09-14 11:25:26 +0530

    added Explicit type casting program

---
+++ Explicit.java
@@ -0,0 +1,7 @@
+public class Explicit {
+    public static void main(String[] args) {
+        int a = 10;
+        double d = (double) a; // explicit type casting
+        System.out.println(d);
+    }
+}
