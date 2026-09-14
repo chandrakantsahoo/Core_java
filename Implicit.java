430d0417e9525514ebf178a2b5b063559b6ef5a6
mrchandrakantsahoo-lab <aapka-github-mrchandrakantsahoo@gmail.com>
2026-09-14 11:14:13 +0530

    added Implicit type casting program

---
+++ Implicit.java
@@ -0,0 +1,8 @@
+// Java program to demonstrate implicit type casting
+public class Implicit {
+    public static void main(String[] args) {
+       double d=10;// implicit type casting
+       System.out.println(d);
+    }
+    
+}
